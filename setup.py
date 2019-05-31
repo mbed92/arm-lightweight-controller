277c64f08b5dba4f9a7c41aab45da6df00ba4c21
mbed92 <michalbednarek.e@gmail.com>
2019-05-31 15:07:18 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ur-lightweight-controller',
+      version='1.0',
+      description='A lightweight controller for manipulators.',
+      author='Michal Bednarek',
+      author_email='michal.bednarek@put.poznan.pl'
+      )
